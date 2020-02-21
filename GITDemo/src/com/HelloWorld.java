7282b228a69535e05dba9d184a6560e2be45bdd4
Aniket Kumar <49431802+theaniketonline@users.noreply.github.com>
2020-02-21 17:46:46 +0530

    Third Change

--- GITDemo/src/com/HelloWorld.java
+++ GITDemo/src/com/HelloWorld.java
@@ -3,8 +3,6 @@ package com;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hi All!!!");
-		System.out.println("Hi World!!!");
 		System.out.println("Hello World!!!");
 		System.out.println("Welcome To Java World!!!");
 	}
