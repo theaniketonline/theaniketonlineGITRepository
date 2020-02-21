d93ce9cf44e8c68744dc35f9ffbdd2bed6aac79b
theaniketonline <theaniketonline@gmail.com>
2020-02-21 17:37:14 +0530

    Revert "Some More Changes in File."
    This reverts commit b09ad1321c21083d946a106431d3ab3d00f7858b.

--- GITDemo/src/com/HelloWorld.java
+++ GITDemo/src/com/HelloWorld.java
@@ -3,7 +3,6 @@ package com;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hi World!!!");
 		System.out.println("Hello World!!!");
 		System.out.println("Welcome To Java World!!!");
 	}
